9618fff92be39623b702682ec4d114890d45cf43
Sean Lewis <scl63@drexel.edu>
2023-01-20 17:29:32 -0500

    added random parsing of array of card values

--- BlackJack/index.js
+++ BlackJack/index.js
@@ -1,39 +1,39 @@
 
 // First card and second card random number
 
-
+let cardVals = [11,11,11,11,2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10]
 let cardsEl = document.getElementById("cards-el")
 let scoreEl = document.getElementById("score-el")
 let DrawEl = document.getElementById("draw-el")
 let messageEl = document.getElementById("message-el")
 
+function getCard() {
+    var rCard = cardVals[Math.floor(Math.random() * cardVals.length)]
+    return rCard
+}
 
-function startGame() {
-    var firstCard = Math.floor(Math.random() * 11) + 1
-    var secondCard = Math.floor(Math.random() * 11) + 1 
+function startGame(firstCard, secondCard) { 
     var sum = firstCard+secondCard
-    var message = ""
-    var gameState = 0
-    console.log(firstCard, secondCard)
     console.log(sum)
     messageEl.textContent = "Good luck..."
     cardsEl.textContent = "Cards: "
     scoreEl.textContent = "Score: "
     cardsEl.textContent += firstCard + ", " + secondCard
     scoreEl.textContent += sum
-    return sum
+    return {firstCard, secondCard}
 }
 
 function cardDraw() {
     console.log("Draw!")
     var sum = Number(scoreEl.textContent.replace("Score:",""))
-    var thirdCard = Math.floor(Math.random() * 11) + 1
+    var thirdCard = getCard()
     
     cardsEl.textContent += ", " + thirdCard
 
     sum += thirdCard
     console.log(sum)
     scoreEl.textContent = "Score: "
+
     scoreEl.textContent += sum
     if (sum <= 20) {
         messageEl.textContent = "Hit or Stay?"
@@ -45,6 +45,4 @@ function cardDraw() {
         messageEl.textContent = "You lost."
         gameState = -1
     }
-
-    console.log(message)
 }
